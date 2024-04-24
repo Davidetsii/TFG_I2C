c570abd602c4ec38769a67471da1d4052259d93e
Davidetsii <167959618+Davidetsii@users.noreply.github.com>
2024-04-24 13:52:14 +0200

    Add files via upload

--- TFG_I2C/tb/tb_I2C_master.vhd
+++ TFG_I2C/tb/tb_I2C_master.vhd
@@ -14,14 +14,15 @@ architecture Behavioral of tb_I2C_master is
 
     constant CLK_PERIOD : time := (1000000000/C_FREQ_SYS)* 1ns; -- 125 MHz
     constant SCL_PERIOD : time := (1000000/C_FREQ_SCL)* 1us; -- 125 MHz 88
-    signal clk, reset, START, DONE, SDA, SCL   : std_logic;
+    signal clk, reset, START, DONE, SDA, SCL, ack_s, overflow   : std_logic;
     signal R_W          : std_logic := '0';
     signal DATA_SLAVE   : std_logic_vector(7 downto 0) := x"35";
     signal DATA_READ    : std_logic_vector(7 downto 0);
     signal DATA_IN      : std_logic_vector(7 downto 0) := x"5d";
     signal ADDRESS      : std_logic_vector(6 downto 0) := "1010101";
-    signal BYTES_W      : std_logic_vector(1 downto 0) := "10";
+    signal BYTES_W      : std_logic_vector(1 downto 0) := "11";
     signal BYTES_R      : std_logic_vector(1 downto 0) := "00";
+    signal div          : std_logic_vector(1 downto 0);
     
 
 begin
@@ -43,7 +44,10 @@ begin
                     SCL     => SCL,
                     DATA_READ  => DATA_READ,
                     BYTES_W     => BYTES_W,
-                    BYTES_R     => BYTES_R                    
+                    BYTES_R     => BYTES_R,
+                    overflow    => overflow,
+                    div         => div,
+                    ack_s       => ack_s                    
         );
         
     clk_stimuli : process
@@ -68,18 +72,23 @@ begin
             START <= '1';
             wait for CLK_PERIOD + CLK_PERIOD/4;
             START <= '0';
+            for k in 0 to to_integer(unsigned(BYTES_W)) - 1 loop
+                wait until div = "00" and ack_s = '1' and overflow = '1';
+                DATA_IN <= DATA_IN(0)&DATA_IN(7 downto 1);            
+            end loop;            
             wait;
         else
             wait for SCL_PERIOD/8;
             START <= '1';
             wait for CLK_PERIOD + CLK_PERIOD/4;
             START <= '0'; 
-            wait for 20*SCL_PERIOD;
-            DATA_IN <= x"ab";
-            wait for 2*SCL_PERIOD;
-            wait for 11*SCL_PERIOD*(to_integer(unsigned(BYTES_W)));
+            for k in 0 to to_integer(unsigned(BYTES_W)) - 1 loop
+                wait until div = "00" and ack_s = '1' and overflow = '1';
+                DATA_IN <= DATA_IN(0)&DATA_IN(7 downto 1);            
+            end loop;
+            wait for 11*SCL_PERIOD*2;
             
-            wait for 2*SCL_PERIOD + 3*SCL_PERIOD/4 + 999*CLK_PERIOD;
+            wait for 3*SCL_PERIOD +3*SCL_PERIOD/2 + 999*CLK_PERIOD;
             
             for k in 0 to to_integer(unsigned(BYTES_R)) - 1 loop
                 for i in 0 to 6 loop
@@ -89,8 +98,9 @@ begin
                 SDA <= DATA_SLAVE(0);
                 wait for SCL_PERIOD;
                 SDA <= 'Z';   
-                wait for 3*SCL_PERIOD;
+                wait for 2*SCL_PERIOD;
                 DATA_SLAVE <= DATA_SLAVE(6 downto 0)&'1';
+                wait for SCL_PERIOD;
             end loop;                                                           
             wait;
         end if;
