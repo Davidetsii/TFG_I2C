c8d90776ad25f2f2651f3539006258c265e2b967
Davidetsii <167959618+Davidetsii@users.noreply.github.com>
2024-07-15 21:53:55 +0200

    15 de julio
    - sda y scl descritos como triestado cada uno

--- tb/tb_I2C_master.vhd
+++ tb/tb_I2C_master.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity i2c_master_tb is
 --  Port ( );
@@ -10,10 +11,12 @@ architecture Behavioral of i2c_master_tb is
 
     constant CLK_PERIOD : time := 10 ns;
     
-    signal clk, reset_n, start, done, r_w, sda, scl : std_logic;
+    signal clk, reset_n, start, done, r_w, sda_in, sda_out, sda_en, scl_in, scl_out, scl_en : std_logic;
     signal address : std_logic_vector(6 downto 0);
     signal data_in : std_logic_vector(7 downto 0);
     signal data_out : std_logic_vector(15 downto 0);
+    
+    signal scl, sda : std_logic;
 
 begin
 
@@ -29,10 +32,38 @@ begin
             address  => address,
             data_in  => data_in,
             done     => done,
-            sda      => sda,
-            scl      => scl,
+            sda_in   => sda_in,
+            sda_out  => sda_out,
+            sda_en   => sda_en,
+            scl_in   => scl_in,
+            scl_out  => scl_out,
+            scl_en   => scl_en,
             data_out => data_out,
             r_w      => r_w
+    );
+    
+    IOBUF_SCL_inst : IOBUF
+    generic map (
+      DRIVE => 12,
+      IOSTANDARD => "DEFAULT",
+      SLEW => "SLOW")
+    port map (
+      O => scl_in,     -- Buffer output
+      IO => scl,   -- Buffer inout port (connect directly to top-level port)
+      I => scl_out,     -- Buffer input
+      T => scl_en      -- 3-state enable input, high=input, low=output 
+    );
+    
+    IOBUF_SDA_inst : IOBUF
+    generic map (
+      DRIVE => 12,
+      IOSTANDARD => "DEFAULT",
+      SLEW => "SLOW")
+    port map (
+      O => sda_in,     -- Buffer output
+      IO => sda,   -- Buffer inout port (connect directly to top-level port)
+      I => sda_out,     -- Buffer input
+      T => sda_en      -- 3-state enable input, high=input, low=output 
     );
     
     clk_stimuli : process
@@ -53,8 +84,8 @@ begin
         reset_n <= '0';
         start <= '0';
         r_w <= '1';
-        address <= (others => '0');
-        data_in <= (others => '0');
+        address <= "0101010";
+        data_in <= "11001100";
         wait for 5*CLK_PERIOD;
         
         reset_n <= '1';
